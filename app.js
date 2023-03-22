6901c8a9caec23e5169586718c5885873fbc0edd
victor <victordorelmocanu@gmail.com>
2023-03-22 15:44:09 +0100

    add new car to app.js

--- app.js
+++ app.js
@@ -16,5 +16,8 @@ class Car {
 car1 = new Car ("Seat", "Whatever", "Blue", false, true);
 car2 = new Car ("Volvo", "V1", "Green", false, false);
 
+car3 = new Car ("Dacia", "Logan", "Green", false, true);
 
-console.log(car1.color);
+#loren Ipsum bla bla bla
+
+console.log(car1.banana);
